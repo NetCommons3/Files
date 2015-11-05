1b937643b33a3f3b4cf3bbe0a3f4f9b8d7a8e555
RyujiAMANO <ryuji@ryus.co.jp>
2015-11-05 10:52:57 +0900

    scheme.phpのクラス名変更

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -17,7 +17,7 @@
  * @SuppressWarnings(PHPMD.LongVariable)
  * @SuppressWarnings(PHPMD.TooManyFields)
  */
-class AppSchema extends CakeSchema {
+class FilesSchema extends CakeSchema {
 
 /**
  * Database connection
