e0c2d9242f83ee479ec1ca65bb29e1251a4275ea
RyujiAMANO <ryuji@ryus.co.jp>
2015-11-18 15:11:38 +0900

    ルームのガード条件とブロックのガード条件追加

--- Controller/Component/DownloadComponent.php
+++ Controller/Component/DownloadComponent.php
@@ -31,23 +31,69 @@ class DownloadComponent extends Component {
 	}
 
 
+	/**
+	 * ダウンロード
+	 *
+	 * @param $contentId
+	 * @param $fieldName
+	 * @param null $size
+	 * @return mixed
+	 */
 	public function doDownload($contentId, $fieldName, $size = null) {
 
-		// TODO ファイル情報取得 plugin_keyとコンテンツID、フィールドの情報が必要
+		// ファイル情報取得 plugin_keyとコンテンツID、フィールドの情報が必要
 		$UploadFile = ClassRegistry::init('Files.UploadFile');
 		$pluginKey = Inflector::underscore($this->controller->plugin);
 		$file = $UploadFile->getFile($pluginKey, $contentId, $fieldName);
-		// TODO ルームチェック
+		// ルームチェック
+		if($file['UploadFile']['room_id']){
+			$roomId = Current::read('Room.id');
+			if($file['UploadFile']['room_id'] != $roomId){
+				throw new ForbiddenException();
+			}
+		}
+		if($file['UploadFile']['block_key']){
+			// block_keyによるガード
+			$Block = ClassRegistry::init('Blocks.Block');
+			$block = $Block->findByKeyAndLanguageId($file['UploadFile']['block_key'], Current::read('Language.id'));
+			if($this->isVisibleBlock($block) === false){
+				throw new ForbiddenException();
+			}
+		}
 
-		$roomId = Current::read('Room.id');
 		// TODO size対応
-		// TODO path Upload時に確定させる
 
-		//$filePath = WWW_ROOT . 'files/upload_file/real_file_name/' . $roomId . '/' . $file['UploadFile']['id'] . '/' . $file['UploadFile']['real_file_name'];
 		$filePath = WWW_ROOT . $file['UploadFile']['path'] . $file['UploadFile']['id'] . '/' .  $file['UploadFile']['real_file_name'];
 
 		$this->controller->response->file($filePath, array('name' => $file['UploadFile']['original_name']));
 		return $this->controller->response;
+	}
 
+
+	// TODO Blockモデルへ
+	public function isVisibleBlock($block) {
+		switch($block['Block']['public_type']){
+			case 0:
+				// 非表示
+				return false;
+				break;
+			case 1:
+				// 表示
+				return true;
+				break;
+			case 2:
+				// 期間限定
+				$now = NetCommonsTime::getNowDatetime();
+				$start = $block['Block']['from'];
+				$end = $block['Block']['to'];
+				if($start !== null && $start > $now){
+					return false;
+				}
+				if($end !== null && $end < $now){
+					return false;
+				}
+				return true;
+				break;
+		}
 	}
 }
