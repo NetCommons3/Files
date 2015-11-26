a469957e0ef8de63c33b7171a4c20fa26c44b349
RyujiAMANO <ryuji@ryus.co.jp>
2015-11-26 10:50:52 +0900

    ReformatCode

--- Controller/Component/FileUploadComponent.php
+++ Controller/Component/FileUploadComponent.php
@@ -21,7 +21,11 @@ App::uses('TemporaryUploadFile', 'Files.Utility');
  */
 class FileUploadComponent extends Component {
 
-	protected $_files = array();
+/**
+ * @var Controller 呼び出し元コントローラ
+ */
+	protected $_controller;
+
 /**
  * Called before the Controller::beforeFilter().
  *
@@ -29,8 +33,7 @@ class FileUploadComponent extends Component {
  * @return void
  */
 	public function initialize(Controller $controller) {
-		$this->controller = $controller;
-		//$this->_grab();
+		$this->_controller = $controller;
 	}
 
 /**
@@ -40,7 +43,7 @@ class FileUploadComponent extends Component {
  * @return TemporaryUploadFile
  */
 	public function getTemporaryUploadFile($fieldName) {
-		$file = Hash::get($this->controller->request->data, $fieldName);
+		$file = Hash::get($this->_controller->request->data, $fieldName);
 		$fileObject = new TemporaryUploadFile($file);
 		return $fileObject;
 	}
@@ -79,46 +82,4 @@ class FileUploadComponent extends Component {
 
 		return $data;
 	}
-
-//	protected function _grab(){
-//		if(Hash::get($_FILES, 'data', false)){
-//			// ファイルアップロードがある。
-//			// 名前を取得する
-//			// $this->Form->create(false, ['type' => 'file']);
-//			// $this->Form->input('import_csv', ['type' => 'file']); したときに$_FILESの形式
-//			//array(
-//			//		'data' => array(
-//			//				'name' => array(
-//			//						'import_csv' => '',
-//			//						'import_photo' => ''
-//			//				),
-//			//				'type' => array(
-//			//						'import_csv' => '',
-//			//						'import_photo' => ''
-//			//				),
-//			//				'tmp_name' => array(
-//			//						'import_csv' => '',
-//			//						'import_photo' => ''
-//			//				),
-//			//				'error' => array(
-//			//						'import_csv' => (int) 4,
-//			//						'import_photo' => (int) 4
-//			//				),
-//			//				'size' => array(
-//			//						'import_csv' => (int) 0,
-//			//						'import_photo' => (int) 0
-//			//				)
-//			//		)
-//			//);// [data][name][import_csv]
-//			// モデルがあると
-//			// [data][name][ModelName][import_csv]
-//			$fields = array_keys($_FILES['data']['name']);
-//			foreach($fields as $field){
-//				$tmp_name = $this->request->data[$field]['tmp_name'];
-//				$this->_files[] = new TemporaryUploadFile($tmp_name);
-//			}
-//		}
-//
-//debug($_FILES);
-//	}
 }
