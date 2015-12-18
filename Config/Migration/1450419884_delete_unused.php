62fdb6dd447d67595883f75743e899019b83c68e
RyujiAMANO <ryuji@ryus.co.jp>
2015-12-18 15:28:44 +0900

    reformat code

--- Config/Migration/1450419884_delete_unused.php
+++ Config/Migration/1450419884_delete_unused.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * DeleteUnused
+ *
+ * @author   Ryuji AMANO <ryuji@ryus.co.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ */
+
+/**
+ * Class DeleteUnused
+ */
 class DeleteUnused extends CakeMigration {
 
 /**
