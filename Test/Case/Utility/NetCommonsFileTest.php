7a94c5c4dc3581d9e43afeb38418498d43343dc3
RyujiAMANO <ryuji@ryus.co.jp>
2015-12-21 14:02:13 +0900

    reformat code

--- Test/Case/Utility/NetCommonsFileTest.php
+++ Test/Case/Utility/NetCommonsFileTest.php
@@ -18,10 +18,25 @@ class NetCommonsFileTest extends NetCommonsCakeTestCase {
  */
 	public $fixtures = [];
 
+/**
+ * test basename
+ *
+ * @return void
+ */
 	public function testBasename() {
 		$path = TMP . '日本語ファイル名.gif';
 		$basename = NetCommonsFile::basename($path);
 		$this->assertEquals($basename, '日本語ファイル名.gif');
 	}
 
+/**
+ * test basenamae フォルダパス無しでの動作
+ *
+ * @return void
+ */
+	public function testBasenameForOnlyFile() {
+		$path = '日本語ファイル名.gif';
+		$basename = NetCommonsFile::basename($path);
+		$this->assertEquals($basename, '日本語ファイル名.gif');
+	}
 }
