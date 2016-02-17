2495b45a4c359dedd08fd5f674410ce5ad199920
RyujiAMANO <ryuji@ryus.co.jp>
2016-02-17 11:02:08 +0900

    ダウンロードコンポーネントでUploadFile.idを指定してのダウンロードも可能にした

--- Controller/Component/DownloadComponent.php
+++ Controller/Component/DownloadComponent.php
@@ -50,6 +50,47 @@ class DownloadComponent extends Component {
 		$UploadFile = ClassRegistry::init('Files.UploadFile');
 		$pluginKey = Inflector::underscore($this->_controller->plugin);
 		$file = $UploadFile->getFile($pluginKey, $contentId, $fieldName);
+
+		return $this->_downloadUploadFile($file, $size, $options);
+	}
+
+/**
+ * UploadFileのID指定でのダウンロード実行
+ *
+ * @param int $uploadFileId UploadFile ID
+ * @param array $options オプション field : ダウンロードのフィールド名, size: nullならオリジナル thumb, small, medium, big
+ * @return CakeResponse|null
+ * @throws ForbiddenException
+ */
+	public function doDownloadByUploadFileId($uploadFileId, $options = array()) {
+		$fieldName = $this->_controller->params['pass'][2];
+		$size = Hash::get($this->_controller->params['pass'], 3, null);
+
+		$fieldName = Hash::get($options, 'field', $fieldName);
+		unset($options['field']);
+		$size = Hash::get($options, 'size', $size);
+		unset($options['size']);
+
+		// ファイル情報取得 plugin_keyとコンテンツID、フィールドの情報が必要
+		$UploadFile = ClassRegistry::init('Files.UploadFile');
+
+		$file = $UploadFile->findById($uploadFileId);
+
+		return $this->_downloadUploadFile($file, $size, $options);
+	}
+
+/**
+ * ダウンロード処理
+ *
+ * @param array $file UploadFile data
+ * @param string $size サムネイル名
+ * @param array $options オプション
+ * @return CakeResponse|null
+ * @throws ForbiddenException
+ */
+	protected function _downloadUploadFile($file, $size, $options) {
+		$UploadFile = ClassRegistry::init('Files.UploadFile');
+
 		// ルームチェック
 		if ($file['UploadFile']['room_id']) {
 			$roomId = Current::read('Room.id');
@@ -60,7 +101,10 @@ class DownloadComponent extends Component {
 		if ($file['UploadFile']['block_key']) {
 			// block_keyによるガード
 			$Block = ClassRegistry::init('Blocks.Block');
-			$uploadFileBlock = $Block->findByKeyAndLanguageId($file['UploadFile']['block_key'], Current::read('Language.id'));
+			$uploadFileBlock = $Block->findByKeyAndLanguageId(
+				$file['UploadFile']['block_key'],
+				Current::read('Language.id')
+			);
 			if ($Block->isVisible($uploadFileBlock) === false) {
 				throw new ForbiddenException();
 			}
@@ -75,8 +119,10 @@ class DownloadComponent extends Component {
 		$filePath = WWW_ROOT . $file['UploadFile']['path'] . $file['UploadFile']['id'] . DS . $filename;
 
 		$options = Hash::merge(array('name' => $file['UploadFile']['original_name']), $options);
-		$this->_controller->response->file($filePath, $options
-			);
+		$this->_controller->response->file(
+			$filePath,
+			$options
+		);
 
 		// Download カウントアップ
 		$UploadFile->countUp($file);
