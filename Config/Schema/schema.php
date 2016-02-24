9b3d5911756f5b7c53d5cd4f3fa443cccc51f144
RyujiAMANO <ryuji@ryus.co.jp>
2016-02-24 10:42:51 +0900

    Migrationファイルをまとめる

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,55 +1,15 @@
-<?php
-/**
- * Schema file
- *
- * @author Noriko Arai <arai@nii.ac.jp>
- * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @link http://www.netcommons.org NetCommons Project
- * @license http://www.netcommons.org/license.txt NetCommons License
- * @copyright Copyright 2014, NetCommons Project
- */
-
-/**
- * Schema file
- *
- * @author Shohei Nakajima <nakajimashouhei@gmail.com>
- * @package NetCommons\Files\Config\Schema
- * @SuppressWarnings(PHPMD.LongVariable)
- * @SuppressWarnings(PHPMD.TooManyFields)
- */
+<?php 
 class FilesSchema extends CakeSchema {
 
-/**
- * Database connection
- *
- * @var string
- */
 	public $connection = 'master';
 
-/**
- * before
- *
- * @param array $event event
- * @return bool
- */
 	public function before($event = array()) {
 		return true;
 	}
 
-/**
- * after
- *
- * @param array $event event
- * @return void
- */
 	public function after($event = array()) {
 	}
 
-/**
- * upload_files table
- *
- * @var array
- */
 	public $upload_files = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID |  |  | '),
 		'plugin_key' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
@@ -75,11 +35,6 @@ class FilesSchema extends CakeSchema {
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
-/**
- * upload_files_contents table
- *
- * @var array
- */
 	public $upload_files_contents = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'plugin_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
